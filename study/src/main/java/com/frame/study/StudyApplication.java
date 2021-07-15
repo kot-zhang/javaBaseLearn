9791a9a3f26b540104cbe193d8efc77904e0d805
zhanglin <zhanglin_wuhan@yijiupi.cn>
2021-07-15 18:12:05 +0800

    启动类

---
+++ study/src/main/java/com/frame/study/StudyApplication.java
@@ -0,0 +1,25 @@
+package com.frame.study;
+
+import com.frame.study.SpringBeanExtension.Ex_ImportAnno;
+import com.frame.study.designParrent.ChainOfResponsibility.RequestPluginConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@ImportResource(locations = {"classpath:BeanXml.xml"})
+@Ex_ImportAnno
+@EnableAsync
+public class StudyApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(StudyApplication.class, args);
+        RequestPluginConfig requestPluginConfig = context.getBean(RequestPluginConfig.class);
+        requestPluginConfig.createChainWrapper().interceptor();
+    }
+
+}
